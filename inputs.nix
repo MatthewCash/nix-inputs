8d347beee97bc8b4b895933839b48458f042cf46
Matthew_Cash <matthew@matthew-cash.com>
2024-08-02 20:38:40 -0700

    Bump lanzaboote to v0.4.1

--- inputs.nix
+++ inputs.nix
@@ -54,7 +54,7 @@
     };
 
     lanzaboote = {
-        url = "github:nix-community/lanzaboote/v0.3.0";
+        url = "github:nix-community/lanzaboote/v0.4.1";
         inputs.nixpkgs.follows = "nixpkgsStable";
     };
 
